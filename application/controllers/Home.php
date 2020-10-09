ae49abf4ae09c70185d37461f0702af8aa1d3a11
Kevin Ochieng <kevincliffo@gmail.com>
2020-10-09 15:57:47 +0300

    First Commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	public function index()
+	{
+        $data['title'] = 'Capelectrotek &#8739; Creative Business & Consultancy';
+        $data['faviconpartpath'] = base_url().'assets/images/favicon.png';
+
+        $this->load->view('includes/header', $data);
+        $this->load->view('view_home', $data);
+        $this->load->view('includes/footer', $data);	        
+	}
+}
