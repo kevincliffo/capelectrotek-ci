878fe550d30d5bbcda53c7222c21846ea6557d5c
Kevin Ochieng <kevincliffo@gmail.com>
2020-10-10 00:14:49 +0300

    Indenting code

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
         $data['title'] = 'Capelectrotek &#8739; Creative Business & Consultancy';
-        $data['faviconpartpath'] = base_url().'assets/images/favicon.png';
+        $data['faviconpartpath'] = base_url().'assets/img/logo-1.png';
 
         $this->load->view('includes/header', $data);
         $this->load->view('view_home', $data);
