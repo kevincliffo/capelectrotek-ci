535388fdeb80d0824b5aa6cd50b2fe6afdeb0459
Kevin Ochieng <kevincliffo@gmail.com>
2020-10-09 16:16:56 +0300

    Added Logo

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -45,9 +45,9 @@
 
       <div class="row justify-content-center">
         <div class="col-xl-11 d-flex align-items-center">
-          <h1 class="logo mr-auto"><a href="index.html">Capelectrotek</a></h1>
+          <!-- <h1 class="logo mr-auto"><a href="index.html">Capelectrotek</a></h1> -->
           <!-- Uncomment below if you prefer to use an image logo -->
-          <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+          <a href="index.html" class="logo mr-auto"><img src="assets/img/header-2.png" alt="" class="img-fluid"></a>
 
           <nav class="nav-menu d-none d-lg-block">
             <ul>
